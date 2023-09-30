ba11c4f9e16f8c4011d198695e4135cdfa1fa7d7
Paweł Brejnak <pawelbrejnak6@gmail.com>
2023-09-30 14:08:08 +0200

    added basics redux without funcionality

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Tasks from "./features/tasks/Tasks.js";
+import App from "./App.js";
 import reportWebVitals from "./reportWebVitals";
 import { GlobalStyle } from "./styled";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
+import { Provider } from "react-redux";
+import store from "./store.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Tasks />
-    </ThemeProvider>
+    <Provider story={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 

8c74236355da565a2b962c9079f743f8ff477cdc
Paweł Brejnak <pawelbrejnak6@gmail.com>
2023-10-16 22:13:04 +0200

    make authorpage easier to read

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -2,18 +2,16 @@ import Section from "../../common/Section";
 import Header from "../../common/Header";
 import Container from "../../common/Container";
 
-function AuthorPage() {
-  return (
-    <Container>
-      <Header header="O autorze" />
-      <Section
-        title="Paweł Brejnak"
-        body={
-          "Nazywam się Paweł mam 16 lat i uczęszczam do czwartej klasy techinkum o profilu technik informatyk W wolnym czasie lubie grać w gry komputerowe lub grać na pianinie."
-        }
-      />
-    </Container>
-  );
-}
+const AuthorPage = () => (
+  <Container>
+    <Header header="O autorze" />
+    <Section
+      title="Paweł Brejnak"
+      body={
+        "Nazywam się Paweł mam 16 lat i uczęszczam do czwartej klasy techinkum o profilu technik informatyk W wolnym czasie lubie grać w gry komputerowe lub grać na pianinie."
+      }
+    />
+  </Container>
+);
 
 export default AuthorPage;
